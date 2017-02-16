c5f792125abd2c36fa619cef56fda4bad3be91ab
DHDO <dhdo@nx.ad.csbsju.edu>
2017-02-16 12:23:55 -0600

    Terminal 2 in Step 5: Modified Employee.java introduce a conflict

--- EmployeeProject/edu/csbsju/cs/Employee.java
+++ EmployeeProject/edu/csbsju/cs/Employee.java
@@ -30,9 +30,9 @@ public class Employee
       return hireDay;
    }
 
-   public void raiseSalary(double byAbsoluteValue)
+   public void raiseSalary(double byDecimalValue)
    {
-      double raise = salary + 100;
+      double raise;
       salary += raise;
    }
    
